d8450cefaf51f836d11ce0cf606eaac4bf8fa628
Nagaraju0123 <141734111+Nagaraju0123@users.noreply.github.com>
2024-06-09 18:11:01 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import { Container, Row, Col } from "reactstrap";
+//import Login from "./Login";
+import React, { Component } from "react";
+import HooksDeneme from "./HooksDeneme";
+
+export default class App extends Component 
+{
+  state = {};
+
+  render() {
+
+    return (
+      <div>
+        <HooksDeneme/>
+      </div>
+    );
+  }
+}
